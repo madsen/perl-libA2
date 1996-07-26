fafd380add6dd74abed703553e9a555e2364b755
Christopher J. Madsen <perl@cjmweb.net>
1996-07-26 11:24:00 -0500

    Improve AppleII::Disk
    Modified AppleII::Disk:
      Added maxlen
      Changed new to create empty file when writing and file doesn't exist
    Modified AppleII::Disk::ProDOS:
      Added seekBlock & writeBlock
      Changed readBlock to use seekBlock
    Modified AppleII::Disk::DOS33:
      Added seekSector, writeBlock, & writeSector
      Changed readSector to use seekSector

--- lib/AppleII/Disk.pm
+++ lib/AppleII/Disk.pm
@@ -5,7 +5,7 @@ package AppleII::Disk;
 #
 # Author: Christopher J. Madsen <ac608@yfn.ysu.edu>
 # Created: 25 Jul 1996
-# Version: $Revision: 0.1 $ ($Date: 1996/07/26 02:35:51 $)
+# Version: $Revision: 0.2 $ ($Date: 1996/07/26 16:24:00 $)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ require Exporter;
 BEGIN
 {
     # Convert RCS revision number to d.ddd format:
-    ' $Revision: 0.1 $ ' =~ / (\d+)\.(\d{1,3})(\.[0-9.]+)? /
+    ' $Revision: 0.2 $ ' =~ / (\d+)\.(\d{1,3})(\.[0-9.]+)? /
         or die "Invalid version number";
     $VERSION = $VERSION = sprintf("%d.%03d%s",$1,$2,$3);
 } # end BEGIN
@@ -52,6 +52,7 @@ BEGIN
 #   writable:  True if the image is opened in read/write mode
 #   file:      The FileHandle attached to the image file
 #   actlen:    The size of the image file in bytes
+#   maxlen:    The maximum allowable size of the image file in bytes
 #---------------------------------------------------------------------
 # Constructor:
 #
@@ -70,18 +71,23 @@ sub new
     my $self = {};
     $self->{filename} = $filename;
 
+    my $file = new FileHandle;
+
+    $mode = 'r' unless $mode;
     my $openMode = '<';
     if ($mode =~ /w/) {
         $self->{writable} = 1;
         $openMode = '+<';
-    }
+        $file->open(">$filename") or croak("Couldn't create `$filename'")
+            if not -e $filename; # Create empty file
+    } # end if writable
 
-    my $file = new FileHandle;
     $file->open("$openMode$filename") or croak("Couldn't open `$filename'");
     binmode $file;
 
     $self->{file}   = $file;
     $self->{actlen} = (stat $file)[7]; # Get real size of file
+    $self->{maxlen} = $self->{actlen};
     bless $self, $type;
 } # end AppleII::Disk::new
 
@@ -114,7 +120,7 @@ sub new
 # Input:
 #   block:  The block number to read
 #   data:   The contents of the block
-#   pad:    The value to pad the block with (optional)
+#   pad:    A character to pad the block with (optional)
 #     If pad is omitted, an error is generated if data is not 512 bytes
 #
 # Implemented in AppleII::Disk::ProDOS & AppleII::Disk::DOS33
@@ -150,22 +156,61 @@ use integer;
 
 sub readBlock
 {
-    my ($self, $block) = @_;
-    my $file = $self->{file};
+    my $self = shift;
 
-    my $pos = $block * 0x200;
-#    return "\0" x 0x200 if $pos > $self->{actlen}; # Past EOF
-    croak("Invalid block number $block") if $pos < 0 or $pos > $self->{actlen};
-
-    $file->seek($pos,0) or die;
+    return "\0" x 0x200
+        if $self->seekBlock($_[0]) >= $self->{actlen}; # Past EOF
     my $buffer = '';
-    read($file,$buffer,0x200) or die;
+    read($self->{file},$buffer,0x200) or die;
 
     $buffer;
 } # end AppleII::Disk::ProDOS::readBlock
 
 #---------------------------------------------------------------------
 # FIXME AppleII::Disk::ProDOS::readSector not implemented yet
+
+#---------------------------------------------------------------------
+# Seek to the beginning of a block:
+#
+# Input:
+#   block:  The block number to seek to
+#
+# Returns:
+#   The new position of the file pointer
+
+sub seekBlock
+{
+    my ($self, $block) = @_;
+
+    my $pos = $block * 0x200;
+    croak("Invalid block number $block")
+        if $pos < 0 or $pos >= $self->{maxlen};
+
+    $self->{file}->seek($pos,0) or die;
+
+    $pos;
+} # end AppleII::Disk::ProDOS::seekBlock
+
+#---------------------------------------------------------------------
+# Write a block from a ProDOS order disk:
+#
+# See AppleII::Disk::writeBlock
+
+sub writeBlock
+{
+    my ($self, $block, $data, $pad) = @_;
+    croak("Disk image is read/only") unless $self->{writable};
+
+    $data .= $pad x (0x200 - length($data))
+        if (defined $pad and length($data) < 0x200);
+
+    croak(sprintf("Data block is %d bytes",length($data)))
+        unless length($data) == 0x200;
+
+    $self->seekBlock($block);
+    print {$self->{file}} $data or die;
+    $self->{actlen} = (stat $self->{file})[7];
+} # end AppleII::Disk::ProDOS::writeBlock
 
 #=====================================================================
 package AppleII::Disk::DOS33;
@@ -206,8 +251,6 @@ sub readBlock
     my ($self, $block) = @_;
     my ($track, $sector1, $sector2) = block2sector($block);
 
-    print STDERR "Reading block $block\n" if $debug;
-
     $self->readSector($track, $sector1) . $self->readSector($track, $sector2);
 } # end AppleII::Disk::DOS33::readBlock
 
@@ -218,22 +261,78 @@ sub readBlock
 
 sub readSector
 {
-    my ($self, $track, $sector) = @_;
-    my $file = $self->{file};
-
-    my $pos = $track * 0x1000 + $sector * 0x100;
-#    return "\0" x 0x100 if $pos > $self->{actlen}; # Past EOF
-    croak("Invalid position track $track sector $sector")
-        if $pos < 0 or $pos > $self->{actlen};
-
-    print STDERR "Reading track $track sector $sector\n" if $debug;
-
-    $file->seek($pos,0) or die;
+    my $self = shift;
+    return "\0" x 0x100
+        if $self->seekSector(@_[0..1]) >= $self->{actlen}; # Past EOF
     my $buffer = '';
-    read($file,$buffer,0x100) or die;
+    read($self->{file},$buffer,0x100) or die;
 
     $buffer;
-} # end AppleII::Disk::DOS33:readSector
+} # end AppleII::Disk::DOS33::readSector
+
+#---------------------------------------------------------------------
+# Seek to the beginning of a sector:
+#
+# Input:
+#   track:   The track number to seek to
+#   sector:  The sector number to seek to
+#
+# Returns:
+#   The new position of the file pointer
+
+sub seekSector
+{
+    my ($self, $track, $sector) = @_;
+
+    my $pos = $track * 0x1000 + $sector * 0x100;
+    croak("Invalid position track $track sector $sector")
+        if $pos < 0 or $pos >= $self->{maxlen};
+
+    $self->{file}->seek($pos,0) or die;
+    $pos;
+} # end AppleII::Disk::DOS33::seekSector
+
+#---------------------------------------------------------------------
+# Write a sector to a DOS 3.3 order image:
+#
+# See AppleII::Disk::writeSector
+
+sub writeSector
+{
+    my ($self, $track, $sector, $data, $pad) = @_;
+    croak("Disk image is read/only") unless $self->{writable};
+
+    $data .= $pad x (0x100 - length($data))
+        if (defined $pad and length($data) < 0x100);
+
+    croak(sprintf("Data block is %d bytes",length($data)))
+        unless length($data) == 0x100;
+
+    $self->seekSector($track, $sector);
+    print {$self->{file}} $data or die;
+    $self->{actlen} = (stat $self->{file})[7];
+} # end AppleII::Disk::DOS33::writeSector
+
+#---------------------------------------------------------------------
+# Write a block to a DOS33 order disk:
+#
+# See AppleII::Disk::writeBlock
+
+sub writeBlock
+{
+    my ($self, $block, $data, $pad) = @_;
+    croak("Disk image is read/only") unless $self->{writable};
+    my ($track, $sector1, $sector2) = block2sector($block);
+
+    $data .= $pad x (0x200 - length($data))
+        if (defined $pad and length($data) < 0x200);
+
+    croak(sprintf("Data block is %d bytes",length($data)))
+        unless length($data) == 0x200;
+
+    $self->writeSector($track, $sector1, substr($data,0,0x100));
+    $self->writeSector($track, $sector2, substr($data,0x100,0x100));
+} # end AppleII::Disk::DOS33::writeBlock
 
 #=====================================================================
 # Package Return Value:
