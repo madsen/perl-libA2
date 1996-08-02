89cdab73c2cf016223900cd29c1f07b627fc3444
Christopher J. Madsen <perl@cjmweb.net>
1996-08-02 11:07:13 -0500

    Add type, get, & put commands
    Opens disk image with write permission
    Add getFile & putFile

--- bin/prodos.pl
+++ bin/prodos.pl
@@ -1,16 +1,16 @@
 #!perl
 #---------------------------------------------------------------------
-# $Id: prodos.pl,v 0.2 1996/07/31 19:57:29 Madsen Exp $
+# $Id: prodos.pl,v 0.3 1996/08/02 16:07:13 Madsen Exp $
 # Copyright 1996 Christopher J. Madsen
 #
 # A command-line shell for accessing ProDOS disk images
 #---------------------------------------------------------------------
 
-use AppleII::ProDOS 0.006;
+use AppleII::ProDOS 0.012;
 use Term::ReadLine;
 
 my $term = Term::ReadLine->new('ProDOS Shell');
-my $vol  = AppleII::ProDOS->new($ARGV[0]);
+my $vol  = AppleII::ProDOS->new($ARGV[0],'w');
 
 print $vol->directory,"\n";
 
@@ -24,14 +24,57 @@ while (1) {
     last if $cmd =~ /^q(?:uit)?$/;
     eval {
       CMD: {
-        print($vol->directory,"\n"),      next CMD if $cmd eq 'pwd';
-        print($vol->directory($arg),"\n"),next CMD if $cmd eq 'cd';
-        print($vol->catalog,"\n"),        next CMD if $cmd eq 'cat';
+        print($vol->directory,"\n"),        next CMD if $cmd eq 'pwd';
+        print($vol->directory($arg),"\n"),  next CMD if $cmd eq 'cd';
+        print($vol->catalog,"\n"),          next CMD if $cmd eq 'cat';
+        print($vol->getFile($arg)->asText), next CMD if $cmd eq 'type';
+        getFile($vol,$arg),                 next CMD if $cmd eq 'get';
+        putFile($vol,$arg),                 next CMD if $cmd eq 'put';
         print "Bad command `$cmd'\a\n";
       } # end CMD
-    };
+    }; # end eval
     if ($@) {
-        $@ =~ /^(.+) at \Q$0\E line \d+$/ or die $@;
+        $@ =~ /^(.+) at \Q$0\E line \d+\.?$/ or die $@;
         print $1,"\a\n";
     }
 } # end forever
+
+exit;
+
+#=====================================================================
+# Subroutines:
+#---------------------------------------------------------------------
+sub getFile
+{
+    my ($vol, $arg) = @_;
+
+    die "$arg already exists" if -e $arg;
+
+    my $file = $vol->getFile($arg);
+
+    open(OUT, ">$arg") or die;
+    binmode OUT;
+    print OUT $file->data;
+    close OUT;
+} # end getFile
+
+#---------------------------------------------------------------------
+sub putFile
+{
+    my ($vol, $arg) = @_;
+
+    open(IN,"<$arg") or die;
+    binmode IN;
+    my $size = (stat IN)[7];
+    my $data = '';
+    read(IN, $data, $size) == $size or die;
+    close IN;
+
+    my $file = AppleII::ProDOS::File->new($arg, $data);
+    if ($arg =~ /\.s[hd]k$/i) {
+        $file->type(0xE0);
+        $file->auxtype(0x8102);
+    }
+
+    $vol->putFile($file);
+} # end putFile
