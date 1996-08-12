493364a54f6bcca622300705046f22cc0eb5f3f0
Christopher J. Madsen <perl@cjmweb.net>
1996-08-12 18:26:50 -0500

    Build README after fetching files

--- fetch.cmd
+++ fetch.cmd
@@ -1,6 +1,6 @@
 @echo off
 rem ======================================================================
-rem = $Id: fetch.cmd,v 1.1 1996/08/12 18:28:58 Madsen Exp $
+rem = $Id: fetch.cmd,v 1.2 1996/08/12 23:26:50 Madsen Exp $
 rem =
 rem = Collect files for LibA2
 rem ======================================================================
@@ -28,12 +28,16 @@ gosub getFile
 set file=pro_opt.pl
 gosub getFile
 
+ren /q readme.pl README.PL
+echo Building README...
+README.PL
+
 quit 0
 
 :getFile
 iff exist %dest\%file then
   iff %@fileage[%dest\%file] == %@fileage[%source\%file] then
-    echo %file does not need updating
+    echo %file unchanged
     return
   endiff
 endiff
@@ -43,3 +47,7 @@ iff %@attrib[%source\%file, R] != 1 then
 endiff
 vernum.pl -d3 %dest %source\%file
 return
+
+rem Local Variables:
+rem   tmtrack-file-task: "LibA2: fetch.cmd"
+rem End:
