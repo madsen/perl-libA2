8a8d298107c9e9bd8bf33449c8eedd9c6dcbe52e
Christopher J. Madsen <perl@cjmweb.net>
1996-08-12 13:28:58 -0500

    Add fetch.cmd

---
+++ fetch.cmd
@@ -0,0 +1,45 @@
+@echo off
+rem ======================================================================
+rem = $Id: fetch.cmd,v 1.1 1996/08/12 18:28:58 Madsen Exp $
+rem =
+rem = Collect files for LibA2
+rem ======================================================================
+setlocal
+
+set file=Disk.pm
+set source=e:\lib\local\perl\AppleII
+set dest=lib\AppleII
+gosub getFile
+
+set file=ProDOS.pm
+gosub getFile
+
+set source=e:\Util\bin
+set dest=bin
+set file=awp2txt.pl
+gosub getFile
+
+set file=prodos.pl
+gosub getFile
+
+set file=pro_fmt.pl
+gosub getFile
+
+set file=pro_opt.pl
+gosub getFile
+
+quit 0
+
+:getFile
+iff exist %dest\%file then
+  iff %@fileage[%dest\%file] == %@fileage[%source\%file] then
+    echo %file does not need updating
+    return
+  endiff
+endiff
+iff %@attrib[%source\%file, R] != 1 then
+  echo %file not locked!
+  quit 1
+endiff
+vernum.pl -d3 %dest %source\%file
+return
