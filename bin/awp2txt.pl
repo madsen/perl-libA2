cb7611a2c2e1d142d039cf5f6a5ce880b06bc701
Christopher J. Madsen <perl@cjmweb.net>
1997-02-25 21:38:49 -0600

    Add documentation for awp2txt.pl
    Give reason for die

--- bin/awp2txt.pl
+++ bin/awp2txt.pl
@@ -1,6 +1,6 @@
 #!perl
 #---------------------------------------------------------------------
-# $Id: awp2txt.pl,v 0.4 1996/08/12 21:13:52 Madsen Exp $
+# $Id: awp2txt.pl,v 0.5 1997/02/26 03:38:49 Madsen Exp $
 # Copyright 1996 Christopher J. Madsen
 #
 # This program is free software; you can redistribute it and/or modify
@@ -15,25 +15,24 @@
 #---------------------------------------------------------------------
 
 foreach $filename (@ARGV) {
-    die unless -f $filename;
+    die "$filename: not a normal file" unless -f $filename;
     if (-e "$filename.txt") {
         print STDERR "$filename.txt already exists, skipping it...\n";
         next;
     }
 
-    open(IN,"<$filename") or die;
+    open(IN,"<$filename") or die "Can't open `$filename'";
     binmode IN;
 
     my $header = '';
-    read(IN,$header,300) == 300 or die;
+    read(IN,$header,300) == 300 or die "$filename: unexpected end-of-file";
 
     if (substr($header,4,1) ne "\x4F") {
         print STDERR "$filename is not an AppleWorks word processor file\n";
         next;
     }
 
-    open(OUT,">$filename.txt") or die;
-    binmode OUT;                # Don't write CRLF
+    open(OUT,">$filename.txt") or die "Can't write `$filename.txt'";
 
     print STDERR "Converting $filename to $filename.txt...\n";
 
@@ -46,7 +45,7 @@ foreach $filename (@ARGV) {
 
     while (1) {
         my $record = '';
-        read(IN,$record,2) == 2 or die;
+        read(IN,$record,2) == 2 or die "$filename: unexpected end-of-file";
         my ($byte0, $byte1) = unpack('C2',$record);
         if ($byte1 == 0xD0) {
             ++$line;
@@ -58,18 +57,53 @@ foreach $filename (@ARGV) {
             # Text record
             ++$line;
             my ($data,$byte2,$byte3) = '';
-            read(IN,$data,$byte1*0x100 + $byte0) or die;
+            read(IN,$data,$byte1*0x100 + $byte0) or die"$filename: read error";
             ($byte2,$byte3,$data) = unpack('C2A*',$data);
             next if $byte2 == 0xFF; # Tab ruler
             # Untabify & delete special codes:
             $data =~ tr/\x16\x17\x01-\x1F/  /d;
             print OUT ' ' x ($byte2 & 0x7F), $data, "\n";
         } else {
-            die sprintf("Unknown record type %02X%02X after line %d",
-                        $byte0, $byte1, $line);
+            die sprintf("%s: unknown record type %02X%02X after line %d",
+                        $filename, $byte0, $byte1, $line);
         }
     } # end forever
 
     close IN;
     close OUT;
 } # end foreach $filename
+
+__END__
+
+=head1 NAME
+
+awp2txt - Convert AppleWorks word processor files to text files
+
+=head1 SYNOPSIS
+
+B<awp2txt> FILE [FILE ...]
+
+=head1 DESCRIPTION
+
+B<awp2txt> converts AppleWorks word processor files to plain text
+file.  For each FILE specified on the command line, it writes the
+contents to FILE.txt.  The original files are not changed.  If
+FILE.txt already exists, it is I<not> overwritten.
+
+=head1 REQUIREMENTS
+
+B<awp2txt> is a stand-alone utility.
+
+=head1 BUGS
+
+There are no known bugs.
+
+=head1 AUTHOR
+
+Christopher J. Madsen E<lt>F<ac608@yfn.ysu.edu>E<gt>
+
+=cut
+
+# Local Variables:
+# tmtrack-file-task: "LibA2: awp2txt.pl"
+# End:
