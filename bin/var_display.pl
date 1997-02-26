ee8d5acc89eed1869f1ca77ff698e24f7b82963e
Christopher J. Madsen <perl@cjmweb.net>
1997-02-25 20:31:29 -0600

    Add documentation for var_display.pl
    Add GPL notice
    Give reason for die

--- bin/var_display.pl
+++ bin/var_display.pl
@@ -1,7 +1,15 @@
 #!perl
 #---------------------------------------------------------------------
-# $Id: var_display.pl,v 1.3 1996/11/01 00:15:00 Madsen Exp $
+# $Id: var_display.pl,v 1.4 1997/02/26 02:31:29 Madsen Exp $
 # Copyright 1996 Christopher J. Madsen
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
+# GNU General Public License or the Artistic License for more details.
 #
 # Display the contents of an Apple II Applesoft BASIC VAR file
 #
@@ -12,11 +20,11 @@
 use strict;
 use vars qw(*ARG);
 
-open(IN,"<$ARGV[0]") or die;
+open(IN,"<$ARGV[0]") or die "Unable to open `$ARGV[0]'";
 binmode IN;
 
 my $header = '';
-read(IN,$header,5) == 5 or die;
+read(IN,$header,5) == 5 or die "Short file";
 
 my ($varSize,$simpleSize,$himem) = unpack('SSC',$header);
 
@@ -116,3 +124,44 @@ sub parseName
 
     ($name, $type);
 } # end parseName
+
+__END__
+
+=head1 NAME
+
+var_display - Display the strings stored in an Applesoft BASIC VAR file
+
+=head1 SYNOPSIS
+
+B<var_display> VAR-FILE
+
+=head1 DESCRIPTION
+
+B<var_display> lists the contents of an Applesoft BASIC VAR file.
+Currently, it can only display string variables and string arrays.
+The strings are formatted ready to drop into a C or Perl program, with
+Control-M (the Apple's end-of-line character) displayed as C<\n>,
+backslashes and quotation marks quoted (C<\\> and C<\">), and control
+characters in the range \x00 to \x1F displayed in hexadecimal
+notation.
+
+=head1 REQUIREMENTS
+
+B<var_display> is a stand-alone utility.
+
+=head1 BUGS
+
+B<var_display> only displays string variables and one-dimensional
+string arrays.  It wouldn't be hard to add support for integer and
+floating-point variables or multi-dimensional arrays, but I don't need
+it.  If you do, send email!
+
+=head1 AUTHOR
+
+Christopher J. Madsen E<lt>F<ac608@yfn.ysu.edu>E<gt>
+
+=cut
+
+# Local Variables:
+# tmtrack-file-task: "LibA2: var_display.pl"
+# End:
