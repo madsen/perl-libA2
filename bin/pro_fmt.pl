e34965c6315d8ab7a6ff6ff4ac1a3cecb8c280fb
Christopher J. Madsen <perl@cjmweb.net>
2006-03-22 20:25:55 -0600

    Add command line options to pro_fmt.pl
      Can call fully_allocate to maximize the image size

--- bin/pro_fmt.pl
+++ bin/pro_fmt.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 #---------------------------------------------------------------------
 # $Id$
-# Copyright 1996 Christopher J. Madsen
+# Copyright 1996-2006 Christopher J. Madsen
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
@@ -14,8 +14,56 @@
 # Create a blank ProDOS disk image
 #---------------------------------------------------------------------
 
-use AppleII::ProDOS 0.016;
+use AppleII::ProDOS 0.04;
+use Getopt::Long 2.10;
+use strict;
 
-our $VERSION = '0.003';
+our $VERSION = '0.04';
 
-my $vol = AppleII::ProDOS->new(@ARGV);
+#---------------------------------------------------------------------
+my $blocks = 280;
+my $volume = 'NEWDISK';
+my $order  = '';
+my ($extendToFullSize, $force);
+
+Getopt::Long::config(qw(bundling no_getopt_compat));
+GetOptions(
+    'blocks|b=i'      => \$blocks,
+    'dos-order|d'     => sub { $order = 'd' },
+    'extend|e'        => \$extendToFullSize,
+    'force|f'         => \$force,
+    'prodos-order|p'  => sub { $order = 'p' },
+    'volume|v=s'      => \$volume,
+    'help'            => \&usage,
+    'version'         => \&usage
+) or usage();
+
+my $filename = shift @ARGV;
+defined($filename) or usage();
+
+sub usage {
+    print "pro_fmt $VERSION\n";
+    exit if $_[0] and $_[0] eq 'version';
+    print "\n" . <<'';
+Usage:  pro_fmt [options] FILE
+  -b, --blocks=SIZE  Make the image SIZE blocks (default 280)
+  -d, --dos-order    Make a DOS 3.3-order image file
+  -e, --extend       Extend FILE to its maximum size
+  -f, --force        Overwrite an existing file
+  -p, --prodos-order Make a ProDOS-order image file
+  -v, --volume=NAME  Use NAME for the ProDOS volume name (default NEWDISK)
+      --help         Display this help message
+      --version      Display version information
+
+    exit;
+} # end usage
+
+#=====================================================================
+if (not $force and -e $filename) {
+  print STDERR "pro_fmt: $filename already exists (use --force to overwrite)\n";
+  exit 1;
+}
+
+my $vol = AppleII::ProDOS->new($volume, $blocks, $filename, $order);
+
+$vol->disk->fully_allocate if $extendToFullSize;
